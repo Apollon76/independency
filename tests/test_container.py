dbd64c82d184acf3ea67aa52800803f37402e210
a.shibaev <a.shibaev@tinkoff.ru>
2021-07-15 01:40:06 +0500

    add overrides

--- tests/test_container.py
+++ tests/test_container.py
@@ -341,3 +341,28 @@ def test_cyclic_dependencies():
     builder.register(B, B, is_singleton=False)
     with pytest.raises(ContainerError):
         builder.build()
+
+
+def test_overridden():
+    class A:
+        def __init__(self, x: int, y: str):
+            self.x = x
+            self.y = y
+
+    builder = ContainerBuilder()
+    builder.singleton(int, lambda: 1)
+    builder.singleton('y', lambda: 'abacaba')
+    builder.singleton(A, A, y=Dep('y'))
+    container = builder.build()
+    container_copy = container.with_overridden_singleton(int, lambda: 2)
+
+    inst = container.resolve(A)
+    assert isinstance(inst, A)
+    assert inst.x == 1
+    assert inst.y == 'abacaba'
+
+    copy_inst = container_copy.resolve(A)
+    assert inst is not copy_inst
+    assert isinstance(copy_inst, A)
+    assert copy_inst.x == 2
+    assert copy_inst.y == 'abacaba'
